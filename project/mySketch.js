c76f474dafea5cf1cd8bc8f21489f0d2593d5838
hayden-34 <26hayden.allen@lwhs.org>
2025-01-28 23:20:54 +0000

    submitting

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,54 @@
+
+
+/*function setup(){
+  createCanvas(400, 400)
+}
+
+function draw(){
+  background(200)
+  ellipse(mouseX, mouseY, 20, 20)
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let myFont;
+let sentence = "Hello, how are you doing today? I brought cookies."
+let sentenceArray = sentence.split(' ')
+let letItGo;
+
+
 function setup() {
   createCanvas(400, 400);
+  myFont = loadFont("knewave.otf");
+  letItGo = loadStrings("letItGo.txt")
+
+  for(let i in sentenceArray){
+    console.log(sentenceArray[i])
+  }
 }
 
 function draw() {
   background(200);
+  textFont(myFont)
+  //textAlign(CENTER, CENTER)
+  text(letItGo, 20, 20, 300)
+
   
 }
